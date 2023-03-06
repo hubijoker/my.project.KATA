6a68921354f75675f80bc6beacb7d9d4776c5998
Руслан <hubijoker@gmail.com>
2023-03-06 16:11:37 +0300

    git commit

--- src/Main.java
+++ src/Main.java
@@ -37,20 +37,29 @@ public class Main {
 
             } catch (Exception e) {
                 try {
-                    String[] rim = {"I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"};
+                    String[] rim = {"I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X",
+                            "XI", "XII", "XIII", "XIV", "XV", "XVI", "XVII", "XVIII", "XIX", "XX",
+                            "XXI", "XXII", "XXIII", "XXIV", "XXV", "XXVI", "XXVII", "XXVIII", "XXIX", "XXX",
+                            "XXXI", "XXXII", "XXXIII", "XXXIV", "XXXV", "XXXVI", "XXXVII", "XXXVIII", "XXXIX", "XL",
+                            "XLI", "XLII", "XLIII", "XLIV", "XLV", "XLVI", "XLVII", "XLVIII", "XLIX", "L",
+                            "LI", "LII", "LIII", "LIV", "LV", "LVI", "LVII", "LVIII", "LIX", "LX",
+                            "LXI", "LXII", "LXIII", "LXIV", "LXV", "LXVI", "LXVII", "LXVIII", "LXIX", "LXX",
+                            "LXXI", "LXXII", "LXXIII", "LXXIV", "LXXV", "LXXVI", "LXXVII", "LXXVIII", "LXXIX", "LXXX",
+                            "LXXXI", "LXXXII", "LXXXIII", "LXXXIV", "LXXXV", "LXXXVI", "LXXXVII", "LXXXVIII", "LXXXIX", "XC",
+                            "XCI", "XCII", "XCIII", "XCIV", "XCV", "XCVI", "XCVII", "XCVIII", "XCIX", "C"};
                     for (int i = 1; i < 11; i++) {
                         for (int j = 1; j < 11; j++) {
                             if (op.equals("-") && x.equals(rim[i-1]) && y.equals(rim[j-1]))
-                                return "" + (i - j);
+                                return rim[(i - j)-1];
 
                             if (op.equals("+") && x.equals(rim[i-1]) && y.equals(rim[j-1]))
-                                return "" + (i + j);
+                                return rim[(i + j)-1];
 
                             if (op.equals("*") && x.equals(rim[i-1]) && y.equals(rim[j-1]))
-                                return "" + (i * j);
+                                return rim[(i * j)-1];
 
                             if (op.equals("/") && x.equals(rim[i-1]) && y.equals(rim[j-1]))
-                                return "" + (i / j);
+                                return rim[(i / j)-1];
 
                             /*если введены римские числа решил вписать их  в массив чтобы было легче
                             задавать им значения сразу внутри цикла for, и возвращаю их резултат после сравнения с
